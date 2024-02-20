20f972897d14d138874acc31f44d435b56e1bb38
walee-vizz <muhammadwaleedkhan052@gmail.com>
2024-02-20 23:28:31 +0500

    first commit

---
+++ resources/js/Components/Counter.vue
@@ -0,0 +1,28 @@
+
+<script setup>
+import { ref } from "vue";
+
+let count = ref(0);
+</script>
+
+<template>
+  <div class="text-center">
+    <h4 class="text-danger">The current count is</h4>
+    <h4>{{ count }}</h4>
+    <!-- <button class="btn btn-red-500 mx-1 w-25">-</button> -->
+    <button
+      @click="count--"
+      class="bg-transparent hover:bg-red-500 text-white-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded"
+    >
+      -
+    </button>
+    <button
+      @click="count++"
+      class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mx-2"
+    >
+      +
+    </button>
+
+    <!-- <button class="btn btn-success mx-1 w-25" @click="count++">+</button> -->
+  </div>
+</template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,8 @@
+<script setup>
+import NavBar from '@/Components/frontend/NavBar.vue';
+
+</script>
+
+<template>
+<NavBar title="Home Page" />
+</template>
